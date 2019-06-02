22ac1ed3ede334d6fd7b87187c4376dbace1b813
Chanon Jenakom <chanonjenakom@gmail.com>
2019-06-02 16:18:42 +0700

    Create Currency model

---
+++ domain/src/main/java/com/mickeycj/domain/models/Currency.kt
@@ -0,0 +1,10 @@
+package com.mickeycj.domain.models
+
+/**
+ * Represents Currency model.
+ */
+data class Currency(
+    val name: String = "",
+    val symbol: String = "",
+    val code: String = ""
+)

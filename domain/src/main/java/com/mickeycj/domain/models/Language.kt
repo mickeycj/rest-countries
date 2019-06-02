bea1cb6b008c69994cd589241b1a0eebfd1b4841
Chanon Jenakom <chanonjenakom@gmail.com>
2019-06-02 16:17:06 +0700

    Create Language model

---
+++ domain/src/main/java/com/mickeycj/domain/models/Language.kt
@@ -0,0 +1,10 @@
+package com.mickeycj.domain.models
+
+/**
+ * Represents Language model.
+ */
+data class Language(
+    val name: String = "",
+    val nativeName: String = "",
+    val iso639Code: String = ""
+)

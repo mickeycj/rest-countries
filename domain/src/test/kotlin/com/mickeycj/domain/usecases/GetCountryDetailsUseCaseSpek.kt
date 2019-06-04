0e6f6a5f03ed75992827fec57b74fd400d5c0fed
Chanon Jenakom <chanonjenakom@gmail.com>
2019-06-05 02:36:47 +0700

    Add spek for 'Get Country Details' use case

--- domain/src/test/kotlin/com/mickeycj/domain/usecases/GetCountryDetailsUseCaseSpek.kt
+++ domain/src/test/kotlin/com/mickeycj/domain/usecases/GetCountryDetailsUseCaseSpek.kt
@@ -1,10 +1,39 @@
 package com.mickeycj.domain.usecases
 
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+
+import com.mickeycj.domain.Faker
+import com.mickeycj.domain.contracts.CountryRepository
 
 /**
  * Spek tests for getting country details from the repository.
  */
 object GetCountryDetailsUseCaseSpek : Spek({
 
+    describe("Get Country Details") {
+
+        val countryRepository by memoized { mockk<CountryRepository>() }
+
+        val getCountryDetailsUseCase by memoized { GetCountryDetailsUseCase(countryRepository) }
+
+        context("When getting country details from the repository") {
+            it("It should return the specified country details") {
+                val code = "USA"
+
+                every { countryRepository.getCountry(code) } returns Faker.getCountry()
+
+                getCountryDetailsUseCase.execute(code)
+
+                verify { countryRepository.getCountry(code) }
+
+                confirmVerified(countryRepository)
+            }
+        }
+    }
 })

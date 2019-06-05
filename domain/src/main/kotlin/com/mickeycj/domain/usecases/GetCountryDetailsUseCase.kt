c37400cabda693993bd246dfa3806e36bfda39cf
Chanon Jenakom <chanonjenakom@gmail.com>
2019-06-06 01:08:00 +0700

    Implement Domain Use Cases (#3)
    Implement Domain Use Cases:
    
    - Get Countries
    - Get Country Details

---
+++ domain/src/main/kotlin/com/mickeycj/domain/usecases/GetCountryDetailsUseCase.kt
@@ -0,0 +1,14 @@
+package com.mickeycj.domain.usecases
+
+import io.reactivex.Single
+
+import com.mickeycj.domain.contracts.CountryRepository
+import com.mickeycj.domain.models.Country
+
+/**
+ * Get country details from the repository.
+ */
+class GetCountryDetailsUseCase(private val countryRepository: CountryRepository) {
+
+    fun execute(code: String): Single<Country> = countryRepository.getCountry(code)
+}

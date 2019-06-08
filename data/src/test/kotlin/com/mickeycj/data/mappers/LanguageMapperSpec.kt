e4ace9c7c5e981683fb0c109c70faace5feb91e4
Chanon Jenakom <chanonjenakom@gmail.com>
2019-06-09 04:26:32 +0700

    Add spec for Language mapper

--- data/src/test/kotlin/com/mickeycj/data/mappers/LanguageMapperSpec.kt
+++ data/src/test/kotlin/com/mickeycj/data/mappers/LanguageMapperSpec.kt
@@ -1,10 +1,27 @@
 package com.mickeycj.data.mappers
 
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+import org.assertj.core.api.Assertions.assertThat
+
+import com.mickeycj.data.MockData
 
 /**
  * Spek tests for Language mapper.
  */
 class LanguageMapperSpec : Spek({
 
+    describe("Language Mapper") {
+
+        val languageMapper by memoized { LanguageMapper() }
+
+        context("When mapping language's data to its corresponding model") {
+            it("It should return the correct model mapping") {
+                val language = languageMapper.toModel(MockData.languageData)
+
+                assertThat(language).isEqualTo(MockData.language)
+            }
+        }
+    }
 })

6a0d1a1e598f763b483508f03be747e9ea932df6
Chanon Jenakom <chanonjenakom@gmail.com>
2019-06-09 04:23:36 +0700

    Add spec for Currency mapper

--- data/src/test/kotlin/com/mickeycj/data/mappers/CurrencyMapperSpec.kt
+++ data/src/test/kotlin/com/mickeycj/data/mappers/CurrencyMapperSpec.kt
@@ -1,10 +1,27 @@
 package com.mickeycj.data.mappers
 
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+import org.assertj.core.api.Assertions.assertThat
+
+import com.mickeycj.data.MockData
 
 /**
  * Spek tests for Currency mapper.
  */
 class CurrencyMapperSpec : Spek({
 
+    describe("Currency Mapper") {
+
+        val currencyMapper by memoized { CurrencyMapper() }
+
+        context("When mapping currency's data to its corresponding model") {
+            it("It should return the correct model mapping") {
+                val currency = currencyMapper.toModel(MockData.currencyData)
+
+                assertThat(currency).isEqualTo(MockData.currency)
+            }
+        }
+    }
 })

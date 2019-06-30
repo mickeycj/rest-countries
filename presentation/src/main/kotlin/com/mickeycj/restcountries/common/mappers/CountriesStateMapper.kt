e6e570a7e924191548d229bc78518706c2ffefe8
Chanon Jenakom <chanonjenakom@gmail.com>
2019-07-01 01:28:33 +0700

    Implement state mappers

--- presentation/src/main/kotlin/com/mickeycj/restcountries/common/mappers/CountriesStateMapper.kt
+++ presentation/src/main/kotlin/com/mickeycj/restcountries/common/mappers/CountriesStateMapper.kt
@@ -8,8 +8,18 @@ import com.mickeycj.restcountries.common.states.CountriesState
 /**
  * State mapper for converting list of Country models to Countries state.
  */
-class CountriesStateMapper :
-    StateMapper<List<Country>, CountriesState> {
+class CountriesStateMapper : StateMapper<List<Country>, CountriesState> {
 
-    override fun toState(from: List<Country>): CountriesState = TODO()
+    override fun toState(from: List<Country>): CountriesState = CountriesState(
+        from.groupBy {
+            it.region
+        }
+        .entries
+        .map { entry ->
+            CountriesState.Region(
+                entry.key,
+                entry.value.map { country -> CountriesState.Country(country.flagUrl, country.code) }
+            )
+        }
+    )
 }
--- presentation/src/main/kotlin/com/mickeycj/restcountries/common/mappers/CountryDetailsStateMapper.kt
+++ presentation/src/main/kotlin/com/mickeycj/restcountries/common/mappers/CountryDetailsStateMapper.kt
@@ -3,13 +3,33 @@ package com.mickeycj.restcountries.common.mappers
 import com.mickeycj.domain.models.Country
 
 import com.mickeycj.restcountries.common.base.StateMapper
+import com.mickeycj.restcountries.common.states.CountriesState
 import com.mickeycj.restcountries.common.states.CountryDetailsState
 
 /**
  * State mapper for converting Country model to Country Details state.
  */
-class CountryDetailsStateMapper :
-    StateMapper<Country, CountryDetailsState> {
+class CountryDetailsStateMapper : StateMapper<Country, CountryDetailsState> {
 
-    override fun toState(from: Country): CountryDetailsState = TODO()
+    override fun toState(from: Country): CountryDetailsState = CountryDetailsState(
+        from.flagUrl,
+        from.name,
+        from.code,
+        from.region,
+        from.capital,
+        from.demonym,
+        from.languages.joinToString(",") { it.name },
+        from.currencies.joinToString(",") { "${it.code}(${it.symbol})" },
+        from.latitude,
+        from.longitude,
+        from.area,
+        from.population,
+        from.giniIndex,
+        from.borders.map {
+            CountriesState.Country(
+                from.flagUrl.replace(from.code.toLowerCase(), it.toLowerCase()),
+                it
+            )
+        }
+    )
 }
